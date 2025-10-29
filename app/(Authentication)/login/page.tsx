973a4328867018bf166eb6777714f51e3e443173
Rikon07 <mh13mahedi@gmail.com>
2025-10-29 13:43:07 +0600

    Auth Layout Login Page

--- app/(Authentication)/login/page.tsx
+++ app/(Authentication)/login/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+// import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-export default function page() {
+
+// export default function LoginPage() {
+//   return (
+//     <Card>
+//         <CardHeader>
+//             <CardTitle className="text-xl">Welcome Bhai</CardTitle>
+//             <CardDescription>Login with your Github</CardDescription>
+//         </CardHeader>
+//     </Card>
+//   )
+// }
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
   return (
-    <div>
-      <h1>Login Page Bhai</h1>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <LoginForm />
+      </div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+//layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <p>Kire Bhai</p>
         {children}
       </body>
     </html>
